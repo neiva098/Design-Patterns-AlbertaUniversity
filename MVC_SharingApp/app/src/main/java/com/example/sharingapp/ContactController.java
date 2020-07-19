6ce2052b354e90bc26f4b8084e9434126ae51449
Cristiano <neivacristiano@yahoo.com.br>
2020-07-19 19:32:25 -0300

    MVC-Sharing_App

---
+++ MVC_SharingApp/app/src/main/java/com/example/sharingapp/ContactController.java
@@ -0,0 +1,43 @@
+package com.example.sharingapp;
+
+import java.util.Observer;
+
+public class ContactController {
+    private Contact contact;
+
+    public ContactController(Contact contact){
+        this.contact = contact;
+    }
+
+    public String getId(){
+        return contact.getId();
+    }
+
+    public void setId(){
+        contact.setId();
+    }
+
+    public void updateId(String id){
+        contact.updateId(id);
+    }
+
+    public String getUsername(){
+        return contact.getUsername();
+    }
+
+    public void setUsername(String username){
+        contact.setUsername(username);
+    }
+
+    public String getEmail(){
+        return contact.getEmail();
+    }
+
+    public void setEmail(String email){
+        contact.setEmail(email);
+    }
+
+    public void addContactObserver(Observer observer){
+        contact.addObserver(observer);
+    }
+}
